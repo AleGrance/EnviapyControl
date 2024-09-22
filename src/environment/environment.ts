2db72bb869b15c292e9d9cae7f17c34d85bbbec9
AleGrance <alegrance1000@gmail.com>
2024-09-22 12:24:48 -0400

    upd server name n domain

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiKey: 'ptZnHK8JlnYSFb1Lv6QPxlXN559bAi21',
-  url: 'https://enviapywwa.online/enviapy'
+  url: 'https://enviapyserver.site/enviapyapi'
   // url: 'http://localhost:3000/api'
 };
